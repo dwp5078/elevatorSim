40e58ebe735f0f775d296f38770b2407754ceb35
Max DeLiso <maxdeliso@gmail.com>
2012-09-26 04:34:29 -0400

    reshuffled links and whitespace

--- README.md
+++ README.md
@@ -8,12 +8,11 @@ A program which provides an extensible and customizable simulation of elevators.
 link
 ----
 Newest code is always available here: https://github.com/maxdeliso/elevatorSim
+Issues are tracked here:              https://github.com/maxdeliso/elevatorSim/issues
 
 directories
 -----------
-
 /deps includes pre-cooked binaries for tier one platforms.
-
 /inc includes header files for this project and its dependencies.
 
 building
--- elevatorSim.cpp
+++ elevatorSim.cpp
@@ -20,21 +20,22 @@ const static char   ELEVATOR_SIM_VERSION[] = "v0.0.0-pre";
 
 int main(int argc, char** argv) {
    Fl_Window *win = new Fl_Window(
-      ELEVATOR_SIM_WINDOW_WIDTH, 
-      ELEVATOR_SIM_WINDOW_HEIGHT, 
+      ELEVATOR_SIM_WINDOW_WIDTH,
+      ELEVATOR_SIM_WINDOW_HEIGHT,
       ELEVATOR_SIM_TITLE);
-   
+
    Fl_Text_Display *disp = new Fl_Text_Display(
-      ELEVATOR_SIM_MARGIN, 
-      ELEVATOR_SIM_MARGIN, 
-      ELEVATOR_SIM_WINDOW_WIDTH - (2 * ELEVATOR_SIM_MARGIN), 
+      ELEVATOR_SIM_MARGIN,
+      ELEVATOR_SIM_MARGIN,
+      ELEVATOR_SIM_WINDOW_WIDTH - (2 * ELEVATOR_SIM_MARGIN),
       ELEVATOR_SIM_WINDOW_HEIGHT - (2 * ELEVATOR_SIM_MARGIN));
-      
-   Fl_Text_Buffer *tbuff = new Fl_Text_Buffer();	
+
+   Fl_Text_Buffer *tbuff = new Fl_Text_Buffer();
+
    disp->buffer(tbuff);
    tbuff->text(ELEVATOR_SIM_VERSION);
-
    win->resizable(*disp);
    win->show();
+   
    return(Fl::run());
 }
