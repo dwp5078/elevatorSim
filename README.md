161bf0442d45f6f2b0b82e27ac0ce12b1376b740
Max DeLiso <maxdeliso@gmail.com>
2012-09-26 00:32:29 -0400

    added new build system and all dependent libs for x86.msvc10.win32

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-There's nothing here
-====================
+elevatorSim
+===========
 
-not even a single pancake.
+The goal of this project is to produce a program which provides an extensible and customizable simulation of elevators.
+Newest code is always available here: https://github.com/maxdeliso/elevatorSim
+
+The structure of the project directory is as follows:
+
+./elevatorSim
++---/deps
++---/inc
++[source]
++[project files]
+
+/deps includes pre-cooked binaries for tier one platforms.
+/inc includes header files for this project and its dependencies.
+
+See INSTALL for building instructions.
--- elevatorSim.cpp
+++ elevatorSim.cpp
@@ -1,5 +1,40 @@
-#include <iostream>
 
-int main() {
-    return 0;
+
+#include <Windows.h> 
+// #include <unistd.h> 
+// TODO: switch on this with macro
+
+#include <iostream>
+#include <boost/thread.hpp>
+#include <sqlite/sqlite3.h>
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Text_Display.H>
+
+const static int    ELEVATOR_SIM_WINDOW_WIDTH = 640;
+const static int    ELEVATOR_SIM_WINDOW_HEIGHT = 480;
+const static int    ELEVATOR_SIM_MARGIN = 20;
+const static char   ELEVATOR_SIM_TITLE[] = "elevatorSim";
+const static char   ELEVATOR_SIM_VERSION[] = "v0.0.0-pre";
+
+int main(int argc, char** argv) {
+   Fl_Window *win = new Fl_Window(
+      ELEVATOR_SIM_WINDOW_WIDTH, 
+      ELEVATOR_SIM_WINDOW_HEIGHT, 
+      ELEVATOR_SIM_TITLE);
+   
+   Fl_Text_Display *disp = new Fl_Text_Display(
+      ELEVATOR_SIM_MARGIN, 
+      ELEVATOR_SIM_MARGIN, 
+      ELEVATOR_SIM_WINDOW_WIDTH - (2 * ELEVATOR_SIM_MARGIN), 
+      ELEVATOR_SIM_WINDOW_HEIGHT - (2 * ELEVATOR_SIM_MARGIN));
+      
+   Fl_Text_Buffer *tbuff = new Fl_Text_Buffer();	
+   disp->buffer(tbuff);
+   tbuff->text(ELEVATOR_SIM_VERSION);
+
+   win->resizable(*disp);
+   win->show();
+   return(Fl::run());
 }
