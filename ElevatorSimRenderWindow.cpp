16ac1889098b959fb88558b2ce4c164856fee1f2
Max DeLiso <maxdeliso@gmail.com>
2012-10-06 16:01:44 -0400

    prevented close dialog from blocking event queue

--- ElevatorSimRenderWindow.cpp
+++ ElevatorSimRenderWindow.cpp
@@ -1,31 +1,31 @@
 /*
- * Copyright (c) 2012, Joseph Max DeLiso
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are those
- * of the authors and should not be interpreted as representing official policies,
- * either expressed or implied, of the FreeBSD Project.
- */
+* Copyright (c) 2012, Joseph Max DeLiso
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The views and conclusions contained in the software and documentation are those
+* of the authors and should not be interpreted as representing official policies,
+* either expressed or implied, of the FreeBSD Project.
+*/
 
 #include "ElevatorSim.hpp"
 #include "ElevatorSimRenderWindow.hpp"
@@ -34,25 +34,27 @@
 #include <FL/gl.h>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Gl_Window.H>
+#include <GL/glut.h>
+#include <GL/GLU.h>
 
 namespace elevatorSim {
 
-const int ElevatorSimRenderWindow::LEFT_MARGIN = 120;
-const int ElevatorSimRenderWindow::RIGHT_MARGIN = 8;
-const int ElevatorSimRenderWindow::TOP_MARGIN = 8;
-const int ElevatorSimRenderWindow::BOTTOM_MARGIN = 8;
+   const int ElevatorSimRenderWindow::LEFT_MARGIN = 120;
+   const int ElevatorSimRenderWindow::RIGHT_MARGIN = 8;
+   const int ElevatorSimRenderWindow::TOP_MARGIN = 8;
+   const int ElevatorSimRenderWindow::BOTTOM_MARGIN = 8;
 
-ElevatorSimRenderWindow::ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label) :
+   ElevatorSimRenderWindow::ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label) :
    Fl_Gl_Window(X, Y, W, H, Label) {
-   /* special initialization code */
-}
-
-void ElevatorSimRenderWindow::draw() {
-   if(!valid()) {
-      /* initialize */
+      /* special initialization code */
    }
 
-   /* draw */
-}
+   void ElevatorSimRenderWindow::draw() {
+      if(!valid()) {
+         /* initialize */
+      }
+
+      /* draw */
+   }
 
 } /* namespace elevatorSim */
--- ElevatorSimWindow.cpp
+++ ElevatorSimWindow.cpp
@@ -1,31 +1,31 @@
 /*
- * Copyright (c) 2012, Joseph Max DeLiso
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are those
- * of the authors and should not be interpreted as representing official policies,
- * either expressed or implied, of the FreeBSD Project.
- */
+* Copyright (c) 2012, Joseph Max DeLiso
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The views and conclusions contained in the software and documentation are those
+* of the authors and should not be interpreted as representing official policies,
+* either expressed or implied, of the FreeBSD Project.
+*/
 
 #include "ElevatorSim.hpp"
 #include "ElevatorSimWindow.hpp"
@@ -34,36 +34,81 @@
 #include <FL/Fl.H>
 #include <FL/gl.h>
 #include <FL/Fl_Window.H>
-#include <Fl/fl_ask.h>
+#include <FL/Fl_Button.H>
 
 namespace elevatorSim {
 
-ElevatorSimWindow::ElevatorSimWindow() : Fl_Window(WIDTH, HEIGHT, TITLE) {
-   ElevatorSimRenderWindow *erw = new ElevatorSimRenderWindow(
-   ElevatorSimRenderWindow::LEFT_MARGIN,
-   ElevatorSimRenderWindow::TOP_MARGIN,
-   WIDTH - (ElevatorSimRenderWindow::LEFT_MARGIN + ElevatorSimRenderWindow::RIGHT_MARGIN),
-   HEIGHT - (ElevatorSimRenderWindow::TOP_MARGIN + ElevatorSimRenderWindow::BOTTOM_MARGIN));
-
-   resizable(*erw);
-   end();
-}
-
-int ElevatorSimWindow::handle(int event) {
-    switch(event) {
+   int ElevatorSimWindow::handle(int event) {
+      switch(event) {
       case FL_HIDE:  
-      if(!fl_ask("Are you sure?")) {
-         show();
-      }
-      return 1;
-      
+         if(wantedClose) {
+            return Fl_Window::handle(event);
+         } else {
+            show();
+            showQuitConfirmDialog();
+            return 1;
+         }
       default:
          return Fl_Window::handle(event);
+      }
    }
-}
 
-const char ElevatorSimWindow::TITLE[] = "elevatorSim";
-const int ElevatorSimWindow::WIDTH = 640;
-const int ElevatorSimWindow::HEIGHT = 480;
+   void quitConfirmedCB(Fl_Button* yesButton, void* data) {
+      ElevatorSimWindow* thisWin = (ElevatorSimWindow*) data;
+
+      thisWin->wantedClose = true;
+      thisWin->hideQuitConfirmDialog();
+      thisWin->hide();
+   } 
+
+   void quitCancelledCB(Fl_Button* yesButton, void* data) {
+      ElevatorSimWindow* thisWin = (ElevatorSimWindow*) data;
+      thisWin->wantedClose = false;
+      thisWin->hideQuitConfirmDialog();
+   } 
+
+   void ElevatorSimWindow::showQuitConfirmDialog() {
+      /* Lazy-allocation of quit dialog widgets */
+      if(!confirmDialog && !yesButton && !noButton) {
+         confirmDialog = new Fl_Window(220, 110, "Are you sure?");
+         yesButton = new Fl_Button(10, 10, 200, 40, "yes");
+         noButton = new Fl_Button(10, 60, 200, 40, "no");
+
+         yesButton->callback((Fl_Callback*) quitConfirmedCB, this);
+         noButton->callback((Fl_Callback*) quitCancelledCB, this);
+
+         confirmDialog->add(yesButton);
+         confirmDialog->add(noButton);
+         confirmDialog->end();
+      }
+
+      confirmDialog->show();
+   }
+
+   void ElevatorSimWindow::hideQuitConfirmDialog() {
+      if(confirmDialog) {
+         confirmDialog->hide();
+      }
+   }
+
+   ElevatorSimWindow::ElevatorSimWindow() : Fl_Window(WIDTH, HEIGHT, TITLE) {
+      ElevatorSimRenderWindow *erw = new ElevatorSimRenderWindow(
+         ElevatorSimRenderWindow::LEFT_MARGIN,
+         ElevatorSimRenderWindow::TOP_MARGIN,
+         WIDTH - (ElevatorSimRenderWindow::LEFT_MARGIN + ElevatorSimRenderWindow::RIGHT_MARGIN),
+         HEIGHT - (ElevatorSimRenderWindow::TOP_MARGIN + ElevatorSimRenderWindow::BOTTOM_MARGIN));
+
+      resizable(*erw);
+      end();
+
+      confirmDialog = NULL;
+      yesButton = NULL;
+      noButton = NULL;
+      wantedClose = false;
+   }
+
+   const char ElevatorSimWindow::TITLE[] = "elevatorSim";
+   const int ElevatorSimWindow::WIDTH = 640;
+   const int ElevatorSimWindow::HEIGHT = 480;
 
 } /* namespace elevatorSim */
