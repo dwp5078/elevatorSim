b2534f1905462695633993f75fd4d564a5d41b32
Max DeLiso <maxdeliso@gmail.com>
2012-10-06 17:37:51 -0400

    merge Soohoon's opengl fixes

--- ElevatorSimRenderWindow.cpp
+++ ElevatorSimRenderWindow.cpp
@@ -1,31 +1,31 @@
 /*
- * Copyright (c) 2012, Joseph Max DeLiso
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are those
- * of the authors and should not be interpreted as representing official policies,
- * either expressed or implied, of the FreeBSD Project.
- */
+* Copyright (c) 2012, Joseph Max DeLiso
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The views and conclusions contained in the software and documentation are those
+* of the authors and should not be interpreted as representing official policies,
+* either expressed or implied, of the FreeBSD Project.
+*/
 
 #include "ElevatorSim.hpp"
 #include "ElevatorSimRenderWindow.hpp"
@@ -37,245 +37,244 @@
 
 namespace elevatorSim {
 
-const int ElevatorSimRenderWindow::LEFT_MARGIN = 120;
-const int ElevatorSimRenderWindow::RIGHT_MARGIN = 8;
-const int ElevatorSimRenderWindow::TOP_MARGIN = 8;
-const int ElevatorSimRenderWindow::BOTTOM_MARGIN = 8;
+   const int ElevatorSimRenderWindow::LEFT_MARGIN = 120;
+   const int ElevatorSimRenderWindow::RIGHT_MARGIN = 8;
+   const int ElevatorSimRenderWindow::TOP_MARGIN = 8;
+   const int ElevatorSimRenderWindow::BOTTOM_MARGIN = 8;
 
-ElevatorSimRenderWindow::ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label) :
+   ElevatorSimRenderWindow::ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label) :
    Fl_Gl_Window(X, Y, W, H, Label) {
-   /* special initialization code */
-	   
-	   spin = 0.0;
-		m_vecCamPos.x = 0.f;
-		m_vecCamPos.y = 0.f;
-		m_vecCamPos.z = 20.f;
-		
-		m_vecCamLookAt.x = 0.f;
-		m_vecCamLookAt.y = 0.f;
-		m_vecCamLookAt.z = 0.f;
-	   
-		m_vecCamUp.x = 0.0f;
-		m_vecCamUp.y = 1.0f;
-		m_vecCamUp.z = 0.0f;
+      /* special initialization code */
 
-	   Fl::add_timeout(FPS, Timer_CB, (void*)this);
-}
+      spin = 0.0;
+      m_vecCamPos.x = 0.f;
+      m_vecCamPos.y = 0.f;
+      m_vecCamPos.z = 20.f;
 
-void ElevatorSimRenderWindow::draw() {
-   if(!valid()) {
-      /* initialize */
-	   InitCube();
+      m_vecCamLookAt.x = 0.f;
+      m_vecCamLookAt.y = 0.f;
+      m_vecCamLookAt.z = 0.f;
 
-		GlInit();
-		setViewport();
+      m_vecCamUp.x = 0.0f;
+      m_vecCamUp.y = 1.0f;
+      m_vecCamUp.z = 0.0f;
 
+      Fl::add_timeout(FPS, Timer_CB, (void*)this);
    }
 
-   /* draw */
+   void ElevatorSimRenderWindow::draw() {
+      if(!valid()) {
+         /* initialize */
+         InitCube();
 
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+         GlInit();
+         setViewport();
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+      }
 
-	gluPerspective(45.0f, (GLfloat)w()/(GLfloat)h(), 0.1f, 500.0f);
+      /* draw */
+      glClearColor(0.0, 0.0, 0.0, 0.0);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	gluLookAt(	m_vecCamPos.x, m_vecCamPos.y, m_vecCamPos.z, 
-				m_vecCamLookAt.x, m_vecCamLookAt.y, m_vecCamLookAt.z, 
-				m_vecCamUp.x, m_vecCamUp.y, m_vecCamUp.z);
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
 
-	glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+      gluPerspective(45.0f, (GLfloat)w()/(GLfloat)h(), 0.1f, 500.0f);
 
-	glEnable(GL_LIGHTING);	
-	glEnable(GL_LIGHT0);
+      gluLookAt(	m_vecCamPos.x, m_vecCamPos.y, m_vecCamPos.z, 
+         m_vecCamLookAt.x, m_vecCamLookAt.y, m_vecCamLookAt.z, 
+         m_vecCamUp.x, m_vecCamUp.y, m_vecCamUp.z);
 
-	glTranslatef(0.0f, 0.0f, -2.0f);
-	glRotatef(spin, 0.5, 1.0, 0.0);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
 
-	glCallList(OBJ_CUBE);
+      glEnable(GL_LIGHTING);	
+      glEnable(GL_LIGHT0);
 
+      glTranslatef(0.0f, 0.0f, -2.0f);
+      glRotatef(spin, 0.5, 1.0, 0.0);
 
-	//Render floor
-	glLoadIdentity();
-
-	glTranslatef(0.0f, -2.0f, 0.0f);
-	glScalef(10.0f, 10.0f, 10.0f);
-
-	glDisable(GL_LIGHTING);	
-	glDisable(GL_LIGHT0);
-
-	glBegin(GL_QUADS);
-	glColor3f(0.8f, 0.8f, 0.8f);
-	glNormal3f(0.0f, 1.0f, 0.0f);
-	glVertex3f( 1.0f, 0.0f, -1.0f);
-	glVertex3f(-1.0f, 0.0f, -1.0f);
-	glVertex3f(-1.0f, 0.0f,  1.0f);
-	glVertex3f( 1.0f, 0.0f,  1.0f);
-	glEnd();
+      glCallList(OBJ_CUBE);
 
 
-	GLenum err = glGetError();
-	if ( err != GL_NO_ERROR ) {
-	    fprintf(stderr, "GLGETERROR=%d\n", (int)err);
-	}
-}
+      //Render floor
+      glLoadIdentity();
 
-void ElevatorSimRenderWindow::GlInit() {
-//********************************************
-//Make sure we only do this once
-//********************************************
-	assert(!valid());
-		
-	glEnable(GL_TEXTURE_2D);
-	//********************************************
-	//Misc OpenGL settings
-	//********************************************
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDepthMask(GL_TRUE);
+      glTranslatef(0.0f, -2.0f, 0.0f);
+      glScalef(10.0f, 10.0f, 10.0f);
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 180);
+      glDisable(GL_LIGHTING);	
+      glDisable(GL_LIGHT0);
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light1_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light1_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light1_specular);
-	glLightfv(GL_LIGHT0, GL_POSITION, light1_position);
-	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light1_direction);
-}
+      glBegin(GL_QUADS);
+      glColor3f(0.8f, 0.8f, 0.8f);
+      glNormal3f(0.0f, 1.0f, 0.0f);
+      glVertex3f( 1.0f, 0.0f, -1.0f);
+      glVertex3f(-1.0f, 0.0f, -1.0f);
+      glVertex3f(-1.0f, 0.0f,  1.0f);
+      glVertex3f( 1.0f, 0.0f,  1.0f);
+      glEnd();
 
-void ElevatorSimRenderWindow::setViewport() 
-{
-    glViewport(0, 0, w(), h());
 
-    float ratio = (float)w() / (float)h();
-    Perspective(45.0, 1.0*ratio, 1.0, 200.0);
-}
+      GLenum err = glGetError();
+      if ( err != GL_NO_ERROR ) {
+         fprintf(stderr, "GLGETERROR=%d\n", (int)err);
+      }
+   }
 
-void ElevatorSimRenderWindow::Perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
-    GLdouble xmin, xmax, ymin, ymax;
-    ymax = zNear * tan(fovy * M_PI / 360.0);
-    ymin = -ymax;
-    xmin = ymin * aspect;
-    xmax = ymax * aspect;
-    glFrustum(xmin, xmax, ymin, ymax, zNear, zFar);
-}
+   void ElevatorSimRenderWindow::GlInit() {
+      //********************************************
+      //Make sure we only do this once
+      //********************************************
+      assert(!valid());
 
-void ElevatorSimRenderWindow::Timer_CB(void *userdata)
-{
-	ElevatorSimRenderWindow* myWindow = (ElevatorSimRenderWindow*)userdata;
-	myWindow->spin += 2.0;      //spin
-	myWindow->redraw();
-	Fl::repeat_timeout(FPS, Timer_CB, userdata);
-}
+      glEnable(GL_TEXTURE_2D);
+      //********************************************
+      //Misc OpenGL settings
+      //********************************************
+      glShadeModel(GL_SMOOTH);
+      glEnable(GL_DEPTH_TEST);
+      glDepthFunc(GL_LEQUAL);
+      glDepthMask(GL_TRUE);
 
-void ElevatorSimRenderWindow::InitCube()
-{
-	assert(!valid());
+      glEnable(GL_LIGHTING);
+      glEnable(GL_LIGHT0);
+      glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 180);
 
-	glNewList(OBJ_CUBE, GL_COMPILE);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
+      glLightfv(GL_LIGHT0, GL_AMBIENT, light1_ambient);
+      glLightfv(GL_LIGHT0, GL_DIFFUSE, light1_diffuse);
+      glLightfv(GL_LIGHT0, GL_SPECULAR, light1_specular);
+      glLightfv(GL_LIGHT0, GL_POSITION, light1_position);
+      glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light1_direction);
+   }
 
-	glBegin(GL_QUADS);
+   void ElevatorSimRenderWindow::setViewport() 
+   {
+      glViewport(0, 0, w(), h());
 
-	//********************************************
-	//Front Face
-	//********************************************
-	glColor3f(1.0, 0.0, 0.0); //red
-	glNormal3f(0.0, 0.0, 1.0);
-	glVertex3f(-1.0,  1.0,  1.0);
-	glVertex3f( 1.0,  1.0,  1.0);
-	glVertex3f( 1.0, -1.0,  1.0);
-	glVertex3f(-1.0, -1.0,  1.0);
-	//********************************************
-	//Back Face
-	//********************************************
-	glColor3f(0.0, 1.0, 1.0); //cyn
-	glNormal3f(0.0, 0.0, -1.0);
-	glVertex3f( 1.0,  1.0, -1.0);
-	glVertex3f(-1.0,  1.0, -1.0);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glVertex3f( 1.0, -1.0, -1.0);
-	//********************************************
-	//Top Face
-	//********************************************
-	glColor3f(0.0, 1.0, 0.0); //grn
-	glNormal3f(0.0, 1.0, 0.0);
-	glVertex3f(-1.0,  1.0, -1.0);
-	glVertex3f( 1.0,  1.0, -1.0);
-	glVertex3f( 1.0,  1.0,  1.0);
-	glVertex3f(-1.0,  1.0,  1.0);
-	//********************************************
-	//Bottom Face
-	//********************************************
-	glColor3f(1.0, 0.0, 1.0); //mag
-	glNormal3f(0.0, -1.0, 0.0);
-	glVertex3f( 1.0, -1.0, -1.0);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glVertex3f(-1.0, -1.0,  1.0);
-	glVertex3f( 1.0, -1.0,  1.0);
-	//********************************************
-	//Right face
-	//********************************************
-	glColor3f(0.0, 0.0, 1.0); //blu
-	glNormal3f(1.0, 0.0, 0.0);
-	glVertex3f( 1.0,  1.0,  1.0);
-	glVertex3f( 1.0,  1.0, -1.0);
-	glVertex3f( 1.0, -1.0, -1.0);
-	glVertex3f( 1.0, -1.0,  1.0);
-	//********************************************
-	//Left Face
-	//********************************************
-	glColor3f(1.0, 1.0, 0.0); //yel
-	glNormal3f(-1.0, 0.0, 0.0);
-	glVertex3f(-1.0,  1.0, -1.0);
-	glVertex3f(-1.0,  1.0,  1.0);
-	glVertex3f(-1.0, -1.0,  1.0);
-	glVertex3f(-1.0, -1.0, -1.0);
-	glEnd();
+      float ratio = (float)w() / (float)h();
+      Perspective(45.0, 1.0*ratio, 1.0, 200.0);
+   }
 
-	glEndList();
-}
+   void ElevatorSimRenderWindow::Perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
+      GLdouble xmin, xmax, ymin, ymax;
+      ymax = zNear * tan(fovy * M_PI / 360.0);
+      ymin = -ymax;
+      xmin = ymin * aspect;
+      xmax = ymax * aspect;
+      glFrustum(xmin, xmax, ymin, ymax, zNear, zFar);
+   }
 
-void ElevatorSimRenderWindow::key_pressed(KEY_PRESS k)
-{
-	float move = 0.5f;
-	switch(k)
-	{
-	case KEY_UP:
-		m_vecCamPos.y += move;
-		m_vecCamLookAt.y += move;
-		break;
+   void ElevatorSimRenderWindow::Timer_CB(void *userdata)
+   {
+      ElevatorSimRenderWindow* myWindow = (ElevatorSimRenderWindow*)userdata;
+      myWindow->spin += 2.0;      //spin
+      myWindow->redraw();
+      Fl::repeat_timeout(FPS, Timer_CB, userdata);
+   }
 
-	case KEY_DOWN:
-		m_vecCamPos.y -= move;
-		m_vecCamLookAt.y -= move;
-		break;
+   void ElevatorSimRenderWindow::InitCube()
+   {
+      assert(!valid());
 
-	case KEY_RIGHT:
-		m_vecCamPos.x += move;
-		m_vecCamLookAt.x += move;
-		break;
+      glNewList(OBJ_CUBE, GL_COMPILE);
+      glEnable(GL_LIGHTING);
+      glEnable(GL_LIGHT0);
 
-	case KEY_LEFT:
-		m_vecCamPos.x -= move;
-		m_vecCamLookAt.x -= move;
-		break;
+      glBegin(GL_QUADS);
 
-	case KEY_FORWARD:
-		m_vecCamPos.z -= move;
-		break;
+      //********************************************
+      //Front Face
+      //********************************************
+      glColor3f(1.0, 0.0, 0.0); //red
+      glNormal3f(0.0, 0.0, 1.0);
+      glVertex3f(-1.0,  1.0,  1.0);
+      glVertex3f( 1.0,  1.0,  1.0);
+      glVertex3f( 1.0, -1.0,  1.0);
+      glVertex3f(-1.0, -1.0,  1.0);
+      //********************************************
+      //Back Face
+      //********************************************
+      glColor3f(0.0, 1.0, 1.0); //cyn
+      glNormal3f(0.0, 0.0, -1.0);
+      glVertex3f( 1.0,  1.0, -1.0);
+      glVertex3f(-1.0,  1.0, -1.0);
+      glVertex3f(-1.0, -1.0, -1.0);
+      glVertex3f( 1.0, -1.0, -1.0);
+      //********************************************
+      //Top Face
+      //********************************************
+      glColor3f(0.0, 1.0, 0.0); //grn
+      glNormal3f(0.0, 1.0, 0.0);
+      glVertex3f(-1.0,  1.0, -1.0);
+      glVertex3f( 1.0,  1.0, -1.0);
+      glVertex3f( 1.0,  1.0,  1.0);
+      glVertex3f(-1.0,  1.0,  1.0);
+      //********************************************
+      //Bottom Face
+      //********************************************
+      glColor3f(1.0, 0.0, 1.0); //mag
+      glNormal3f(0.0, -1.0, 0.0);
+      glVertex3f( 1.0, -1.0, -1.0);
+      glVertex3f(-1.0, -1.0, -1.0);
+      glVertex3f(-1.0, -1.0,  1.0);
+      glVertex3f( 1.0, -1.0,  1.0);
+      //********************************************
+      //Right face
+      //********************************************
+      glColor3f(0.0, 0.0, 1.0); //blu
+      glNormal3f(1.0, 0.0, 0.0);
+      glVertex3f( 1.0,  1.0,  1.0);
+      glVertex3f( 1.0,  1.0, -1.0);
+      glVertex3f( 1.0, -1.0, -1.0);
+      glVertex3f( 1.0, -1.0,  1.0);
+      //********************************************
+      //Left Face
+      //********************************************
+      glColor3f(1.0, 1.0, 0.0); //yel
+      glNormal3f(-1.0, 0.0, 0.0);
+      glVertex3f(-1.0,  1.0, -1.0);
+      glVertex3f(-1.0,  1.0,  1.0);
+      glVertex3f(-1.0, -1.0,  1.0);
+      glVertex3f(-1.0, -1.0, -1.0);
+      glEnd();
 
-	case KEY_BACKWARD:
-		m_vecCamPos.z += move;
-		break;
-	}
-}
+      glEndList();
+   }
+
+   void ElevatorSimRenderWindow::key_pressed(KEY_PRESS k)
+   {
+      float move = 0.5f;
+      switch(k)
+      {
+      case KEY_UP:
+         m_vecCamPos.y += move;
+         m_vecCamLookAt.y += move;
+         break;
+
+      case KEY_DOWN:
+         m_vecCamPos.y -= move;
+         m_vecCamLookAt.y -= move;
+         break;
+
+      case KEY_RIGHT:
+         m_vecCamPos.x += move;
+         m_vecCamLookAt.x += move;
+         break;
+
+      case KEY_LEFT:
+         m_vecCamPos.x -= move;
+         m_vecCamLookAt.x -= move;
+         break;
+
+      case KEY_FORWARD:
+         m_vecCamPos.z -= move;
+         break;
+
+      case KEY_BACKWARD:
+         m_vecCamPos.z += move;
+         break;
+      }
+   }
 
 } /* namespace elevatorSim */
--- ElevatorSimRenderWindow.hpp
+++ ElevatorSimRenderWindow.hpp
@@ -6,10 +6,10 @@
 * modification, are permitted provided that the following conditions are met:
 *
 * 1. Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
+*    list of conditions and the following disclaimer.
 * 2. Redistributions in binary form must reproduce the above copyright notice,
-* this list of conditions and the following disclaimer in the documentation
-* and/or other materials provided with the distribution.
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
 *
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -38,37 +38,37 @@
 
 namespace elevatorSim {
 
-class ElevatorSimRenderWindow : public Fl_Gl_Window {
- private:
-   float spin;
-   Vec3f	m_vecCamPos;
-   Vec3f	m_vecCamLookAt;
-   Vec3f	m_vecCamUp;
+   class ElevatorSimRenderWindow : public Fl_Gl_Window {
+   private:
+      float spin;
+      Vec3f	m_vecCamPos;
+      Vec3f	m_vecCamLookAt;
+      Vec3f	m_vecCamUp;
 
-public:
-   void draw();
-   void GlInit();
-   void setViewport();
-   void Perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
-   static void Timer_CB(void *userdata);
-   void key_pressed(KEY_PRESS k);
- 
+   public:
+      void draw();
+      void GlInit();
+      void setViewport();
+      void Perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+      static void Timer_CB(void *userdata);
+      void key_pressed(KEY_PRESS k);
+
    public:
 
-   const static int LEFT_MARGIN;
-   const static int RIGHT_MARGIN;
-   const static int TOP_MARGIN;
-   const static int BOTTOM_MARGIN;
+      const static int LEFT_MARGIN;
+      const static int RIGHT_MARGIN;
+      const static int TOP_MARGIN;
+      const static int BOTTOM_MARGIN;
 
-   
 
-   ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label = 0);
 
-   
+      ElevatorSimRenderWindow(int X, int Y, int W, int H, const char* Label = 0);
 
-private:
-   void InitCube();
-};
+
+
+   private:
+      void InitCube();
+   };
 
 } /* namespace elevatorSim */
 
