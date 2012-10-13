a2eb73af6389da5a5e11b6562b9ebacf158d7509
Max DeLiso <maxdeliso@gmail.com>
2012-10-13 16:26:16 -0400

    formatting fixes and removed win32 dependencies

--- cTimeManager.cpp
+++ cTimeManager.cpp
@@ -1,70 +1,52 @@
 #include "cTimeManager.hpp"
 #include <ctime>
 
-cTimeManager::cTimeManager(void)
-{
+cTimeManager::cTimeManager() {
+
 }
 
-cTimeManager::~cTimeManager(void)
-{
+cTimeManager::~cTimeManager() {
+
 }
 
-cTimeManager* cTimeManager::GetInstance()
-{
-	static cTimeManager Instance;
-	return &Instance;
+cTimeManager* cTimeManager::GetInstance() {
+   static cTimeManager Instance;
+
+   return &Instance;
 }
 
 void cTimeManager::Setup()
 {
-	m_dwCurrTime = clock();
-	m_dwPrevTime = m_dwCurrTime;
+   m_dwCurrTime = clock();
+   m_dwPrevTime = m_dwCurrTime;
 
-	m_dwWorldTime = 0;
-	m_dwElapsedTime = 0;
-	m_dwFps = 0;
-	m_dwFrame = 0;
+   m_dwWorldTime = 0;
+   m_dwElapsedTime = 0;
+   m_dwFps = 0;
+   m_dwFrame = 0;
 
-	m_dwLimitTime = 0;
-	m_dwFpsPlus = 0;
+   m_dwLimitTime = 0;
+   m_dwFpsPlus = 0;
 }
 
 void cTimeManager::Update()
 {
-	m_dwPrevTime = m_dwCurrTime;
-	m_dwCurrTime = clock();
+   m_dwPrevTime = m_dwCurrTime;
+   m_dwCurrTime = clock();
 
-	m_dwElapsedTime = m_dwCurrTime - m_dwPrevTime;
-	m_dwWorldTime += m_dwElapsedTime;
+   m_dwElapsedTime = m_dwCurrTime - m_dwPrevTime;
+   m_dwWorldTime += m_dwElapsedTime;
 
-	if(m_dwLimitTime < m_dwWorldTime)
-	{
-		m_dwLimitTime = m_dwWorldTime + 1000;
-		m_dwFps = m_dwFrame;
-		m_dwFpsPlus += m_dwFrame;
-		m_dwFrame = 0;
-	}
+   if(m_dwLimitTime < m_dwWorldTime)
+   {
+      m_dwLimitTime = m_dwWorldTime + CLOCKS_PER_SEC;
+      m_dwFps = m_dwFrame;
+      m_dwFpsPlus += m_dwFrame;
+      m_dwFrame = 0;
+   }
 
-	else
-	{
-		m_dwFrame++;
-	}
+   else
+   {
+      m_dwFrame++;
+   }
 }
-
-/*void cTimeManager::Render(HDC hdc)
-{
-	char	szBuff[128];
-	SetBkMode(hdc, TRANSPARENT);
-
-	wsprintf(szBuff, "World Time    : %d", m_dwWorldTime);
-	TextOut(hdc, 10, 10, szBuff, strlen(szBuff));
-
-	wsprintf(szBuff, "Elapsed Time  : %d", m_dwElapsedTime);
-	TextOut(hdc, 10, 30, szBuff, strlen(szBuff));
-
-	wsprintf(szBuff, "FPS           : %d", m_dwFps);
-	TextOut(hdc, 10, 50, szBuff, strlen(szBuff));
-
-	wsprintf(szBuff, "Total Frame   : %d", m_dwFpsPlus);
-	TextOut(hdc, 10, 70, szBuff, strlen(szBuff));
-}*/
--- cTimeManager.hpp
+++ cTimeManager.hpp
@@ -1,39 +1,36 @@
 #ifndef __CTIMEMANAGER_H__
 #define __CTIMEMANAGER_H__
 
-#include <windows.h>
-#include <mmsystem.h>
+#include <cstdint>
 
 class cTimeManager
 {
-private:
-	DWORD	m_dwPrevTime;
-	DWORD	m_dwCurrTime;
+   uint32_t m_dwPrevTime;
+   uint32_t m_dwCurrTime;
 
-	DWORD	m_dwWorldTime;
-	DWORD	m_dwElapsedTime;
-	DWORD	m_dwFps;
-	DWORD	m_dwFrame;
+   uint32_t m_dwWorldTime;
+   uint32_t m_dwElapsedTime;
+   uint32_t m_dwFps;
+   uint32_t m_dwFrame;
 
-	DWORD	m_dwLimitTime;
-	DWORD	m_dwFpsPlus;
+   uint32_t m_dwLimitTime;
+   uint32_t m_dwFpsPlus;
 
-	cTimeManager(void);
+   cTimeManager(void);
 
 public:
-	~cTimeManager(void);
+   ~cTimeManager(void);
 
-	static	cTimeManager* GetInstance();
+   static cTimeManager* GetInstance();
 
-	inline	DWORD	GetElapsedTime()	{	return m_dwElapsedTime;	}
-	inline	DWORD	GetWorldTime()		{	return m_dwWorldTime;	}
-	inline	DWORD	GetFPS()			{	return m_dwFps;	}
-   inline   DWORD GetTotalFrame()   {  return m_dwFpsPlus;  }
+   inline uint32_t GetElapsedTime() {  return m_dwElapsedTime; }
+   inline uint32_t GetWorldTime() {  return m_dwWorldTime;   }
+   inline uint32_t GetFPS() {  return m_dwFps;   }
+   inline uint32_t GetTotalFrame() {  return m_dwFpsPlus;  }
 
 
-	void Setup();
-	void Update();
-	//void Render(HDC hdc);
+   void Setup();
+   void Update();
 };
 
 #endif //__CTIMEMANAGER_H__
