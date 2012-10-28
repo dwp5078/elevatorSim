8a4029084675c53cd71d6e807f1ea950e78f019e
Max DeLiso <maxdeliso@gmail.com>
2012-10-28 18:53:10 -0400

    removed unused #undef

--- ElevatorSim.hpp
+++ ElevatorSim.hpp
@@ -35,7 +35,6 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 #elif defined(_ES_DARWIN) || defined(_ES_UNIX)
-#undef M_PI
 #include <unistd.h>
 #else
 #error Unspecified operating system. Use the makefile.
