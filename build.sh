1d4b8935dfc6f2f9c617ddbc5a1136903078dd30
Max DeLiso <maxdeliso@gmail.com>
2012-11-05 22:37:07 -0500

    updated unix style scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+THIS_SYSTEM=$(uname -s)
+
+if [[ "$THIS_SYSTEM" == "Darwin" ]]; then
+make -f makefile.x86_64.darwin.clang3x -j
+elif [[ "$THIS_SYSTEM" == "Linux" ]]; then
 make -f makefile.x86_64.linux.gcc4 -j
+else 
+echo "Couldn't detect OS"
+fi
--- clean.sh
+++ clean.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-make -f makefile.x86_64.linux.gcc4 -j clean
+
+THIS_SYSTEM=$(uname -s)
+
+if [[ "$THIS_SYSTEM" == "Darwin" ]]; then
+make -f makefile.x86_64.darwin.clang3x clean
+elif [[ "$THIS_SYSTEM" == "Linux" ]]; then
+make -f makefile.x86_64.linux.gcc4 clean
+else 
+echo "Couldn't detect OS"
+fi
