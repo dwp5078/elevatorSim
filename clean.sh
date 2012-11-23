b620735aa68c5ac3986000df2244f07f3eb2dc62
Max DeLiso <maxdeliso@gmail.com>
2012-11-23 15:23:30 -0500

    fixed bugs in build scripts

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 THIS_SYSTEM=$(uname -s)
 
-if [[ "$THIS_SYSTEM" == "Darwin" ]]; then
+if [ "$THIS_SYSTEM" == "Darwin" ]; then
 make -f makefile.x86_64.darwin.clang3x clean
-elif [[ "$THIS_SYSTEM" == "Linux" ]]; then
+elif [ "$THIS_SYSTEM" == "Linux" ]; then
 make -f makefile.x86_64.linux.gcc4 clean
 else 
 echo "Couldn't detect OS"
