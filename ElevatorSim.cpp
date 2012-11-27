4da2ce7b6dd68945d8759e8efa5187ae1349ebcc
Max DeLiso <maxdeliso@gmail.com>
2012-11-26 21:36:52 -0500

    remove superfluous pragmas

--- ElevatorSim.cpp
+++ ElevatorSim.cpp
@@ -39,10 +39,7 @@
 #include <boost/thread.hpp>
 #include <boost/chrono.hpp>
 
-#pragma warning (push)
-#pragma warning (disable: 4100)
 #include <boost/program_options.hpp>
-#pragma warning (pop)
 
 #include <iostream>
 #include <iterator>
