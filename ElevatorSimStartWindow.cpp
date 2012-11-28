c789da3e94670e06c5aa34e53ee7e1bbe8e146b4
Max DeLiso <maxdeliso@gmail.com>
2012-11-28 10:32:49 -0500

    added skeleton start window

---
+++ ElevatorSimStartWindow.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2012, Joseph Max DeLiso, Daniel Gilbert
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the FreeBSD Project.
+ */
+
+#include "ElevatorSim.hpp"
+#include "ElevatorSimStartWindow.hpp"
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+
+namespace elevatorSim {
+
+/* public static member initializers */
+const char ElevatorSimStartWindow::WINDOW_TITLE[] = "Start";
+const int ElevatorSimStartWindow::WINDOW_WIDTH = 300;
+const int ElevatorSimStartWindow::WINDOW_HEIGHT = 200;
+
+int ElevatorSimStartWindow::handle(int event) {
+   return Fl_Window::handle(event);
+}
+
+/* public methods */
+ElevatorSimStartWindow::ElevatorSimStartWindow() :
+   Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE) {
+   
+      /* add more widgets to main window here */
+      
+   end();
+}
+
+ElevatorSimStartWindow::~ElevatorSimStartWindow() {
+
+}
+
+} /* namespace elevatorSim */
