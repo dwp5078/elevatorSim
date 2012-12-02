8904141e22f9889e7769cebc19dcbe973271055b
Q115 <richardlegates69@yahoo.com>
2012-12-01 22:03:49 -0500

    Added functionality to elevatorSimStartWindow's accept and cancel button

--- ElevatorSimStartWindow.cpp
+++ ElevatorSimStartWindow.cpp
@@ -42,8 +42,11 @@ namespace elevatorSim {
 
 /* public static member initializers */
 const char ElevatorSimStartWindow::WINDOW_TITLE[] = "Start";
-const int ElevatorSimStartWindow::WINDOW_WIDTH = 300;
-const int ElevatorSimStartWindow::WINDOW_HEIGHT = 200;
+const int ElevatorSimStartWindow::WINDOW_WIDTH = 270;
+const int ElevatorSimStartWindow::WINDOW_HEIGHT = 270;
+int ElevatorSimStartWindow::elevatorNum = 0;
+int ElevatorSimStartWindow::floorNum = 0;
+int ElevatorSimStartWindow::seedNum = 0;
 
 int ElevatorSimStartWindow::handle(int event) {
    return Fl_Window::handle(event);
@@ -55,7 +58,11 @@ void ElevatorSimStartWindow::inputAcceptCB(Fl_Window* w, void* userData) {
       dbgSS << "inputAcceptCB fired with widget ptr " << w << std::endl;
       LOG_INFO( Logger::SUB_FLTK, sstreamToBuffer(dbgSS) );
    }
-
+   ElevatorSimStartWindow* thisWin = (ElevatorSimStartWindow*) userData;
+   elevatorNum = atoi(thisWin->elevatorNumInput->value());
+   floorNum = atoi(thisWin->floorNumInput->value());
+   seedNum = atoi(thisWin->seedNumInput->value());
+   thisWin->hide(); 
 }
 
 void ElevatorSimStartWindow::inputCancelCB(Fl_Window* w, void* userData) {
@@ -65,36 +72,51 @@ void ElevatorSimStartWindow::inputCancelCB(Fl_Window* w, void* userData) {
       LOG_INFO( Logger::SUB_FLTK, sstreamToBuffer(dbgSS) );
    }
    
+   ElevatorSimStartWindow* thisWin = (ElevatorSimStartWindow*) userData;
+   thisWin->hide(); 
 }
 
 /* public methods */
 ElevatorSimStartWindow::ElevatorSimStartWindow() :
    Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE) {
-      elevatorNum = new Fl_Input(100, 20, 140, 30, "# of elevators:");
-      floorNum = new Fl_Input(100, 70, 140, 30, "# of floors:");
-      seedNum = new Fl_Input(100, 120, 140, 30, "Random seed:");
+	  elevatorSim::ElevatorSimStartWindow::elevatorNum = 0;
+      elevatorNumInput = new Fl_Input(100, 20, 140, 30, "# of elevators:");
+      floorNumInput = new Fl_Input(100, 70, 140, 30, "# of floors:");
+      seedNumInput = new Fl_Input(100, 120, 140, 30, "Random seed:");
 
       inputAccept = new Fl_Button(100, 170, 140, 30, "Accept");
       inputCancel = new Fl_Button(100, 220, 140, 30, "Cancel");
       inputAccept->callback((Fl_Callback*) inputAcceptCB, this);
       inputCancel->callback((Fl_Callback*) inputCancelCB, this);
 
-      add(floorNum);
-      add(elevatorNum);
-      add(seedNum);
+      add(floorNumInput);
+      add(elevatorNumInput);
+      add(seedNumInput);
       add(inputCancel);
       add(inputAccept);
    
       end();
 }
 
+int ElevatorSimStartWindow::getElevatorNum() {
+	return elevatorNum;
+}
+
+int ElevatorSimStartWindow::getFloorNum() {
+	return floorNum;
+}
+
+int ElevatorSimStartWindow::getSeedNum() {
+	return seedNum;
+}
+
 ElevatorSimStartWindow::~ElevatorSimStartWindow() {
    delete inputCancel;
    delete inputAccept;
    
-   delete seedNum;
-   delete floorNum;
-   delete elevatorNum;
+   delete seedNumInput;
+   delete floorNumInput;
+   delete elevatorNumInput;
 
    if(isDebugBuild()) {
       std::stringstream dbgSS;
