157c7b27cd2819f41bfb8fed7bf19760ac74c2be
Max DeLiso <maxdeliso@gmail.com>
2012-12-06 17:53:31 -0500

    added results window stub

---
+++ ElevatorSimResultsWindow.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2012, Joseph Max DeLiso
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the FreeBSD Project.
+ */
+
+#include "ElevatorSim.hpp"
+#include "ElevatorSimResultsWindow.hpp"
+#include "SimulationState.hpp"
+#include "Logger.hpp"
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_File_Chooser.H>
+
+#include <boost/lexical_cast.hpp>
+#include <fstream>
+
+namespace elevatorSim {
+
+/* public static member initializers */
+const char ElevatorSimResultsWindow::WINDOW_TITLE[] = "Start";
+const int ElevatorSimResultsWindow::WINDOW_WIDTH = 310;
+const int ElevatorSimResultsWindow::WINDOW_HEIGHT = 320;
+
+/* public methods */
+ElevatorSimResultsWindow::ElevatorSimResultsWindow() :
+   Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE) {
+     
+}
+
+ElevatorSimResultsWindow::~ElevatorSimResultsWindow() {
+   if(isDebugBuild()) {
+      std::stringstream dbgSS;
+      dbgSS << "finished destroying results window @ " << this << std::endl;
+      LOG_INFO( Logger::SUB_MEMORY, sstreamToBuffer(dbgSS) );
+   }
+}
+
+} /* namespace elevatorSim */
